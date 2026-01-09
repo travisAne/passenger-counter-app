4d812add31b78e1daa32e913231eec567b92bf8b
travisAne <travispadera@gmail.com>
2026-01-09 08:30:50 +0200

    onclick btn

--- index.js
+++ index.js
@@ -7,18 +7,6 @@
 //console.log (count)
 //console.log (myAge)
 
-let bonusPoints = 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
-
-
-
-
+function increment() {
+    console.log("The button was clicked")
+}
