26a51ddff7b75c924c33b2a39fc025f76fba3beb
travisAne <travispadera@gmail.com>
2026-01-18 08:31:04 +0200

    previous entires

--- index.js
+++ index.js
@@ -7,18 +7,28 @@
 //console.log (count)
 //console.log (myAge)
 
+let saveEl = document.getElementById("save-el")
+
 let countEl = document.getElementById("count-el")
 
-console.log(countEl)
+
+
+
+console.log(saveEl)
 
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+   countEl.innerText = count
 }
 
 function save() {
+
+   let countStr = count + " - "
+
+    saveEl.innerText += countStr
+
     console.log(count)
 }
 
